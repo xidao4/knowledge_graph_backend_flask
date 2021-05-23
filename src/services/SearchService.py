5dd52aefd875b25ac809cf418b8f533bfab2b99f
wangyuchen <181250147@smail.nju.edu.cn>
2021-05-23 15:42:54 +0800

    fix: 包版本协调

--- src/services/SearchService.py
+++ src/services/SearchService.py
@@ -16,10 +16,3 @@ class SearchService:
             'code': 0,
             'answer': result
         }
-
-
-if __name__ == '__main__':
-    res = que.question_process("章子怡演过多少部电影？")
-    print(res)
-    res = que.question_process("卧虎藏龙是什么类型的电影？")
-    print(res)
